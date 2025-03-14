8d46f41987f6f0204f6a3ce9492a01c56032ef7e
Zachary Patrignani <zachpatrignani@gmail.com>
2025-03-13 22:28:27 -0400

    basic get jobs api

--- server/api/app.py
+++ server/api/app.py
@@ -2,11 +2,13 @@ from flask_cors import cross_origin
 from flask import Flask, request
 from waitress import serve
 import logging
+from api.routes.jobs import jobs_api
 
 log = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.register_blueprint(jobs_api)
 
 def init():
     serve(app, host='0.0.0.0', port=8000)
