284596a8d5f996f487643eb05a28e1bfd573d6f9
Zachary Patrignani <zachpatrignani@gmail.com>
2025-03-15 19:19:21 -0400

    jobs model for client

---
+++ client/src/redux/jobFilters.slice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Define Interface
+export interface JobFiltersState {
+    salary?: number,
+    datePosted?: Date,
+    workType?: string
+}
+
+// Define initial state
+export const initialState: JobFiltersState = {
+    salary: undefined,
+    datePosted: undefined,
+    workType: undefined
+};
+
+// Define slice
+const jobFiltersSlice = createSlice({
+    name : "jobFilters",
+    initialState,
+    reducers: {
+        setSalary(state, action) {
+            state.salary = action.payload;
+        },
+        setDatePosted(state, action) {
+            state.datePosted = action.payload;
+        },
+        setWorkType(state, action) {
+            state.workType = action.payload;
+        }
+    }
+
+});
+
+export const {setSalary, setDatePosted, setWorkType} = jobFiltersSlice.actions;
+export default jobFiltersSlice.reducer;
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import testFeatureReducer from "./TestFeature/TestFeatureSlice";
-
+import jobfiltersReducer from './jobFilters.slice'
 export const store = configureStore({
   reducer: {
     testFeature: testFeatureReducer,
-  },
+    jobFilters: jobfiltersReducer
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
