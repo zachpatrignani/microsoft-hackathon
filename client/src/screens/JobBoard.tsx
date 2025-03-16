e426e4cc4914b36ecbdc6a88de247b0f3c2f820a
Zachary Patrignani <zachpatrignani@gmail.com>
2025-03-15 20:16:01 -0400

    test component

--- client/src/screens/JobBoard.tsx
+++ client/src/screens/JobBoard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './JobBoard.scss';
+import JobFilter from '../components/JobFilter/job-filter';
 
 function JobBoard() {
   return (
-    <div>test</div>
+    <JobFilter />
   );
 }
 
