352e43c5c7a5a0328f9649e1997026f10d898505
Zachary Patrignani <zachpatrignani@gmail.com>
2025-03-17 21:50:47 -0400

    model update

--- client/src/models/job.ts
+++ client/src/models/job.ts
@@ -1,22 +1,22 @@
-
 export type JobWorkType = "Remote" | "On-Site" | "Hybrid";
 export type JobEmploymentType = "Part-Time" | "Full-Time" | "Contract";
 
 export interface Job {
-    _id: string;
-    name: string;
-    company: string;
-    industry?: string;
-    tags?: string[];
-    description: string;
-    city?: string;
-    state?: string;
-    location?: string;
-    _createdAt: string;
-    employmentType: JobEmploymentType;
-    wage: number;
-    workType: JobWorkType;
-    employerId: string;
-    employerPhone?: string;
-    employerEmail?: string;
+  _id: string;
+  name?: string;
+  company?: string;
+  industry?: string;
+  description?: string;
+  skills?: string;
+  responsibilities?: string;
+  city?: string;
+  state?: string;
+  _createdAt?: string;
+  employmentType?: JobEmploymentType;
+  wage?: number;
+  workType?: JobWorkType;
+  employerId?: string;
+  employerPhone?: string;
+  employerEmail?: string;
+  website?: string;
 }
