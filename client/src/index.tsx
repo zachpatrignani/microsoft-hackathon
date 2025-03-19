19a6b627ba92adf2ad9ef0252a73e1793524bcc1
Ariel Magyar <arielamagyar@gmail.com>
2025-03-18 22:59:39 -0400

    Added styling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import  { store }  from './redux/store'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './styles/global.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
