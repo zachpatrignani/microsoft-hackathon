005fc7526635e504f1181b26164d0e3b8cdf3f4e
Maheer Sayeed <maheersayeed@gmail.com>
2025-03-20 12:44:54 -0400

    capped input length to 300 words per field

--- server/services/get_ai_notes.py
+++ server/services/get_ai_notes.py
@@ -3,12 +3,17 @@ import openai
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 import json
+import ast
+import re
+
 
 load_dotenv()
 
+COMPLETIONS_AZURE_ENDPOINT = os.getenv("COMPLETIONS_AZURE_ENDPOINT")
+COMPLETIONS_API_KEY = os.getenv("COMPLETIONS_API_KEY")
+COMPLETIONS_API_VERSION = os.getenv("COMPLETIONS_API_VERSION")
+COMPLETIONS_MODEL=os.getenv("COMPLETIONS_MODEL")
 
-import ast
-import re
 
 def extract_lists_from_string(input_string):
     # Find all Python-style lists in the string using regex
@@ -27,19 +32,6 @@ def extract_lists_from_string(input_string):
 
     return extracted_data
 
-
-
-
-COMPLETIONS_AZURE_ENDPOINT = os.getenv("COMPLETIONS_AZURE_ENDPOINT")
-COMPLETIONS_API_KEY = os.getenv("COMPLETIONS_API_KEY")
-COMPLETIONS_API_VERSION = os.getenv("COMPLETIONS_API_VERSION")
-COMPLETIONS_MODEL=os.getenv("COMPLETIONS_MODEL")
-
-
-print(COMPLETIONS_MODEL)
-print(COMPLETIONS_API_KEY)
-print(COMPLETIONS_AZURE_ENDPOINT)
-print(COMPLETIONS_API_VERSION)
 
 def format_string(note_string, word_limit):
   note_string = note_string.strip()
@@ -67,28 +59,28 @@ def get_notes(preferences, impairments, jobObjectRawString):
 
   jobObject = json.loads(jobObjectRawString)
 
-  description = jobObject["description"]
-  skills = jobObject["skills"]
-  responsibilities = jobObject["responsibilities"]
-  city = jobObject["city"]
-  state = jobObject["state"]
-  workType = jobObject["workType"]
-  employmentType = jobObject["employmentType"]
-  company = jobObject["company"]
+  description = format_string(jobObject["description"], word_limit)
+  skills = format_string(jobObject["skills"], word_limit)
+  responsibilities = format_string(jobObject["responsibilities"], word_limit)
+  city = format_string(jobObject["city"], word_limit)
+  state = format_string(jobObject["state"], word_limit)
+  workType = format_string(jobObject["workType"], word_limit)
+  employmentType = format_string(jobObject["employmentType"], word_limit)
+  company = format_string(jobObject["company"], word_limit)
 
 
 
-  if preferences != "" and impairments != "":
+  if preferences != "NONE" and impairments != "NONE":
     initial_setting = fr"""You are a model that takes user preferences and user impairments as well as job details of a specific job. Your task is to return two lists. One list of reasons why this person will enjoy the job due to their preferences/impairments, and another list of things the user can do to make the job more maangable for them because of their preferences/impairments."""
-  elif preferences != "":
+  elif preferences != "NONE":
     initial_setting = fr"""You are a model that takes user preferences as well job details of a specific job. Your task is to return two lists. One list of reasons why this person will enjoy the job due to their preferences, and another list of things the user can do to make the job managable for them because of their preferences."""
   else:
     initial_setting = fr"""You are a model that takes user impairments as well job details of a specific job. Your task is to return two lists. One list of reasons why this person will enjoy the job due to their impairments, and another list of things the user can do to make the job managable for them because of their preferences."""
 
 
-  if preferences != "" and impairments != "":
+  if preferences != "NONE" and impairments != "NONE":
     user_prompt = fr"""I am looking for a job. My preferences are the following: {preferences}. I have the following impairments: {impairments}."""
-  elif preferences != "":
+  elif preferences != "NONE":
     user_prompt = fr"""I am looking for a job. My preferences are the following: {preferences}."""
   else:
     user_prompt = fr"""I am looking for a job. I have the following impairments: {impairments}."""
@@ -96,9 +88,9 @@ def get_notes(preferences, impairments, jobObjectRawString):
   job_prompt = fr"""The job I am looking at has these details. Description: {description}, responsibilities: {responsibilities}, skills {skills}, type of work: {workType}, type of employment: {employmentType}, city: {city}"""
 
 
-  if preferences != "" and impairments != "":
+  if preferences != "NONE" and impairments != "NONE":
     final_prompt = fr"""Please give me your response formatted as python lists titled matchNotes and jobNotes. Give at most 5 notes per list. If the job is a bad fit, say that include that in the notes."""
-  elif preferences != "":
+  elif preferences != "NONE":
     final_prompt = fr"""Please give me your response formatted as a python list titled matchNotes. Give at most 5 notes. If the job is a bad fit, say that include that in the notes."""
   else:
     final_prompt = fr"""Please give me your response formatted as a python list titled challengeNotes. Give at most 5 notes. If the job is a bad fit, say that include that in the notes."""
@@ -119,7 +111,7 @@ def get_notes(preferences, impairments, jobObjectRawString):
 
 
 
-  print("Maheer", response.choices[0].message.content)
+  print(response.choices[0].message.content)
 
 
   parsed_data = extract_lists_from_string(response.choices[0].message.content)
@@ -130,6 +122,3 @@ def get_notes(preferences, impairments, jobObjectRawString):
     "jobId" :jobObject["_id"]
   }
 
-
-
-# get_notes("", "", "")
